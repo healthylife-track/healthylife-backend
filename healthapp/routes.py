7880102fe65e2692daa81fb3429bea7dc8072c29
Ademola111 <onayemi.hakeem@gmail.com>
2023-11-21 19:12:27 +0100

    initial commit

---
+++ healthapp/routes.py
@@ -0,0 +1,77 @@
+"""to start an app"""
+from flask import render_template, request, flash, redirect, jsonify
+from healthapp import app, db, csrf
+from healthapp.models import User
+from healthapp.signal import contact_signal
+from healthapp.email import send_email_alert
+
+"""homepage"""
+@app.route('/', methods=['GET'])
+def home():
+    if request.method=='GET':
+        return "Welcome to Health Paltform"
+
+"""register"""
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        name=request.form['name']
+        email=request.form['email']
+        role = request.form['role']
+        phoneNo = request.form['phoneno']
+        licenseNo = request.form['licenseNo']
+        bloodgroup = request.form['bloodgroup']
+        # csrf_token = csrf.generate_csrf()
+        # print(csrf_token)
+        if role == 'patient' and licenseNo == 'None':
+            if (name =="" and
+                email =="" and
+                role =="" and
+                phoneNo =="" and
+                bloodgroup ==""):
+                return jsonify({"msg":"One or more field is empty"})
+            elif (name !="" and
+                  email !="" and
+                  role !="" and
+                  phoneNo !="" and
+                  bloodgroup !=""):
+                newuser = User(user_name=name,
+                               user_email=email, 
+                               user_role=role, 
+                               user_phoneNo=phoneNo, 
+                               user_bloodgroup=bloodgroup 
+                               )
+                db.session.add(newuser)
+                return jsonify({"msg":"Registration Successful"})
+        
+        elif role == 'doctor' and licenseNo !='':
+            if (name =="" and
+                email =="" and
+                role =="" and
+                phoneNo =="" and
+                licenseNo =="" and
+                bloodgroup ==""):
+                return jsonify({"msg":"One or more field is empty"})
+            elif (name !="" and
+                  email !="" and
+                  role !="" and
+                  phoneNo !="" and
+                  licenseNo !="" and
+                  bloodgroup !=""):
+                newuser = User(user_name=name,
+                               user_email=email, 
+                               user_role=role, 
+                               user_phoneNo=phoneNo, 
+                               user_bloodgroup=bloodgroup)
+                db.session.add(newuser)
+            return jsonify({"msg":"Registration Successful"})
+
+"""
+
+@contact_signal.connect
+def send_email_alart(sender, comment, post_author_email,  recipients):
+    subject = f"Codinit:New message"
+    body = f"Hi,\n\n My name is {recipients['custom']}, \n\n {comment} \n\n Codinit Team"
+    send_email_alert(subject, body, [post_author_email])
+    
+"""
---
+++ instance/config.py
@@ -0,0 +1,21 @@
+SECRET_KEY="health67&*life"
+
+# mail config
+SECURITY_PASSWORD_SALT = "##Health_&&_world//is@^Life);"
+BCRYPT_LOG_ROUNDS = 13
+WTF_CSRF_ENABLED = True
+DEBUG_TB_ENABLED = False
+DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+# Mail settings
+MAIL_SERVER='smtp.gmail.com'
+MAIL_PORT=465
+MAIL_USE_SSL=True
+MAIL_USE_TLS=False
+
+# gmail authentication
+MAIL_USERNAME='email'
+MAIL_PASSWORD='mm'
+
+# mail account
+MAIL_DEFAULT_SENDER = 'admin@health.com'
